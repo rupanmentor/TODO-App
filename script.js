6c3d78d77c0494ae4dc6fb0563b3ef1f52b39a40
rupanmentor <rupanguvimentor@gmail.com>
2025-10-14 20:42:24 +0530

    styles completed

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
 //! Getting all Elements from html
 
+const form = document.getElementById("form");
+const textInput = document.getElementById("textInput");
+const dateInput = document.getElementById("dateInput");
+const textarea = document.getElementById("textarea");
+const msg = document.getElementById("msg");
+const add = document.getElementById("add");
+const tasks = document.getElementById("tasks");
+
 //! Form Validation
+
+const formValidation = () => {
+  if (
+    textInput.value === "" ||
+    dateInput.value === "" ||
+    textarea.value === ""
+  ) {
+    msg.innerHTML = "Input Fields Cannot Be Empty 😞";
+    //console.log("data not found")
+  } else {
+    msg.innerHTML = "";
+    //console.log("data found");
+    getData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
 
 //! Submit Logic
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 //! Getting details from the form input and storing it in data variable (array of objects)
 
+let data = [{}];
+
+const getData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    task: textarea.value,
+  });
+
+  // to save the data to local storage
+  localStorage.setItem("data", JSON.stringify(data));
+  createTask();
+};
+
 //! Create Function used to create the data from local storage and display it in my todo's
+
+const createTask = () =>{
+    tasks.innerHTML = ""
+    data.map((ele,index)=>{
+        return(
+            tasks.innerHTML += `
+            <div id=${index}> 
+               <span class="fw-bolder">${ele.text}</span>
+                <span class="fw-bolder">${ele.date}</span>
+                <p class="fw-bold">${ele.task}</p>
+                <span class="options">
+                  <i class="fa-solid fa-pen-to-square fa-beat" style="color: #FFD43B;"></i>
+                  <i class="fa-solid fa-trash-can fa-shake" style="color: #FFD43B;"></i>
+                </span>
+            </div>
+            
+            `
+        )
+    })
+}
 
 //! resetting the form after displaying the task
 
