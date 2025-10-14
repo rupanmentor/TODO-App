6943e86ce882a00da969081da6645e2d9ea6937b
rupanmentor <rupanguvimentor@gmail.com>
2025-10-14 19:33:46 +0530

    script steps added

---
+++ script.js
@@ -0,0 +1,15 @@
+//! Getting all Elements from html
+
+//! Form Validation
+
+//! Submit Logic
+
+//! Getting details from the form input and storing it in data variable (array of objects)
+
+//! Create Function used to create the data from local storage and display it in my todo's
+
+//! resetting the form after displaying the task
+
+//! Edit function for created TODO'S
+
+//! Delete Function for created Todo's
